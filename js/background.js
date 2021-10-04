accbc8ec0d09c329a335e3736e0db0d3e9deb049
Shir Raanan <fnuckale@gmail.com>
2021-10-04 22:48:32 +0300

    Prevent background from executing on other pages

--- js/background.js
+++ js/background.js
@@ -1,8 +1,10 @@
-chrome.tabs.onUpdated.addListener((tabId) => {
-    chrome.scripting.executeScript({
-        target: { tabId },
-        func: () => {
-            document.dispatchEvent(new Event("ready:r20poc"));
-        },
-    });
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+    if (tab.url.indexOf("https://app.roll20.net/editor/") == 0) {
+        chrome.scripting.executeScript({
+            target: { tabId },
+            func: () => {
+                document.dispatchEvent(new Event("ready:r20poc"));
+            },
+        });
+    }
 });
